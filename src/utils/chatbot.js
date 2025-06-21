7491ae2874a1e1b787d04d36893a75ce4802dca5
Jonathan Ortega <132107501+Jonnhyortega@users.noreply.github.com>
2025-06-21 18:52:46 -0300

    update project

--- src/utils/chatbot.js
+++ src/utils/chatbot.js
@@ -1,20 +1,29 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
+let firstInteraction = true;
 
 export async function sendMessage(message) {
   const isLocal = window.location.hostname === 'localhost';
   const api_url_intern = isLocal
     ? 'http://localhost:8000/api/chat/'
     : API_URL;
-    const response = await fetch(api_url_intern, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json', 
-        },
-        body: JSON.stringify({ message }),
-    });
 
-    if (!response.ok) {
+  const payload = { message };
+  if (!firstInteraction) {
+    payload.history = [];
+  }
+
+  firstInteraction = false;
+
+  const response = await fetch(api_url_intern, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  });
+
+  if (!response.ok) {
     const errorText = await response.text();
     throw new Error(`Error ${response.status}: ${errorText}`);
   }
